50f80c7ac2c9001bb63b22406e1dd5853e41cd6f
iD Crypt3 <idcrypt3@gmail.com>
2019-07-08 11:33:45 -0700

    Aiden Create UserStory.py
    THis is the description of my project it is funny story i think u like very much. THe password is easy no overthink mucher than you need.

---
+++ Aiden/UserStory.py
@@ -0,0 +1,10 @@
+password=input("What is thee password?")
+if password == "dragon":
+    print("There was once a happy dragon who lived in the forest.")
+    print("He loved to play games with his dad.")
+    print("There was no one that could hurt them.")
+    print("One day, his father got killed in a gang fight.")
+    print("He became depressed.")
+    print("To help him cope with his depression, he started drinking.")
+    print("He developed a serious drinking problem.")
+    print("The dragon died of alcohol overdose and all of the villager imprisoned in his dungeon were free to escape.")
