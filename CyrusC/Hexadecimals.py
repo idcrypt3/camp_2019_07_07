3047e5ee1eefd8536e52c686806159ff4dc13615
iD Crypt3 <idcrypt3>
2019-07-09 09:59:36 -0700

    Cyrus Hexadecimals.py

--- CyrusC/Hexadecimals.py
+++ CyrusC/Hexadecimals.py
@@ -1,2 +1,14 @@
 A = 0x5f
+B = 0b1000_0010_0100_0000_1010_0111_1001_1100
+C = 0x82_40_a7_9c
+
+print(A)
+print(hex(A))
 print(bin(A))
+
+if B == C:
+    print("they are the same")
+    print(B)
+    print(C)
+else:
+    print("they are different")
